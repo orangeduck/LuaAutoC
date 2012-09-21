b3dcec9ec5ac8d471424301bb90a08cfa9458d4a
Jérémy Rosen <jeremy.rosen@enst-bretagne.fr>
2012-09-21 11:19:57 +0200

    change the API to return if we have a push function instead of the function itself

--- src/lautoc_stack.c
+++ src/lautoc_stack.c
@@ -251,12 +251,14 @@ int luaA_push_void(lua_State* L,const void* c_in) {
   return 1;
 }
 
-luaA_Pushfunc luaA_type_push_func(luaA_Type id) {
-  if (id == -1) return NULL;
-  return luaA_hashtable_get(push_table, luaA_type_name(id));
+int luaA_type_has_push_func(luaA_Type id) {
+  if (id == -1) return 0;
+  if(luaA_hashtable_get(push_table, luaA_type_name(id))) return 1;;
+  return 0;
 }
 
-luaA_Tofunc luaA_type_to_func(luaA_Type id) {
-  if (id == -1) return NULL;
-  return luaA_hashtable_get(to_table, luaA_type_name(id));
+int luaA_type_has_to_func(luaA_Type id) {
+  if (id == -1) return 0;
+  if(luaA_hashtable_get(push_table, luaA_type_name(id))) return 1;;
+  return 0;
 }
